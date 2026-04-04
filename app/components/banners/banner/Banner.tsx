7ffff01b0c82b1d6e8d7ee72581c7d3f7e30e3be
Samir Azimov <samirazimov488@gmail.com>
2026-04-05 01:06:31 +0500

    added btn polygon

--- app/components/banners/banner/Banner.tsx
+++ app/components/banners/banner/Banner.tsx
@@ -1,9 +1,11 @@
 import CryptoList from "@/app/components/CryptoList";
 import {getCryptoMarkets} from "@/app/lib/api/crypto";
+import Link from "next/link";
 
 
 const bannerContent = {
-    title: "Beyond crypto. Trade global assets with USDT."
+    title: "Beyond crypto. Trade global assets with USDT.",
+    btnText: "Polygon soon",
 }
 
 
@@ -15,8 +17,11 @@ export default  async function Banner(){
     return (
         <section className="invest__banner min-h-screen py-16 md:py-24">
             <div className="max-w-307.5 mx-auto md:px-6 px-4 w-full h-full">
-                <div className="grid grid-cols-2 gap-3  max-[768px]:grid-cols-1 ">
-                    <h2 className={'text-5xl max-[768px]:text-3xl font-black max-w-137.5 leading-16'}>{bannerContent.title}</h2>
+                <div className="grid grid-cols-2 gap-6  max-[768px]:grid-cols-1 ">
+                    <div className={'flex items-center gap-4 flex-col'}>
+                        <h2 className={'text-5xl max-[768px]:text-3xl font-black max-w-137.5 leading-16'}>{bannerContent.title}</h2>
+                        <Link href={'/'} className={'mt-10 bg-linear-to-br from-[#b21fcc] via-[#222426] to-[#b21fcc] w-full text-white text-center py-5 px-10'}>{bannerContent.btnText}</Link>
+                    </div>
                     <CryptoList items={crypto} />
                 </div>
             </div>
