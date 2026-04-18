b7603be93598005f145a9b82fed0764346f2ae99
Samir Azimov <samirazimov488@gmail.com>
2026-04-18 18:28:20 +0500

    removed buy sell func

--- app/api/close/route.ts
+++ app/api/close/route.ts
@@ -1,24 +1,2 @@
-import { getAccount , updateBalance , removePosition  } from "@/app/lib/store";
 
-
-function calcPnL(pos , price){
-    if(pos.side == "LONG") {
-        return (price - pos.price) * pos.size
-    }
-    return (pos.entry - price) * pos.size
-}
-
-
-export async function POST(req: Request) {
-    const {index , price } = await req.json();
-
-    const account = getAccount()
-    const pos = account.positions[index]
-
-    const pnl = calcPnL(pos , price);
-
-    updateBalance(pnl)
-    removePosition(index)
-    return Response.json({ pnl })
-}
 
