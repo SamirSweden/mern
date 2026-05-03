4f4cfc3ebf37dd72d670958a1b064dc2fc08bbab
Samir Azimov <samirazimov488@gmail.com>
2026-05-04 01:25:21 +0500

    added footer

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -1,6 +1,7 @@
 import BannerText from "../components/banners/listing/BannerText";
 import ListingBanner from "../components/banners/listing/ListingBanner";
 import TokensBanner from "@/app/components/banners/listing/TokensBanner";
+import Footer from "@/app/components/shared/footer/Footer";
 
 
 
@@ -9,6 +10,9 @@ const ListingPage = () => {
         <ListingBanner />
         <BannerText />
         <TokensBanner />
+        <div className="max-w-4xl mx-auto md:px-6 px-4 w-full h-full">
+            <Footer />
+        </div>
     </>
 }
 
