55ee1a966a35549dfe5b4dd137f0a4e7c179b13d
Samir Azimov <samirazimov488@gmail.com>
2026-05-23 22:49:23 +0500

    fixed + added btn for mobile

--- app/room/page.tsx
+++ app/room/page.tsx
@@ -22,10 +22,29 @@ export default function Room(){
 
     return (
         <>
-            <section className={'flex items-center justify-center h-screen'}>
-                <div className={'flex items-center flex-col gap-3 w-75'}>
-                    <div className={'flex flex-col gap-6 items-center '}>
-                        <Image src={krakenImgLogo} alt="kraken"  width={150} height={150} />
+            <section className={'flex items-center justify-center h-screen '}>
+                <div className={'flex items-center flex-col gap-3 w-75 '}>
+                    <div className={'flex flex-col gap-6 items-center relative'}>
+                        <video
+                            className="w-40"
+                            autoPlay
+                            loop
+                            muted
+                            playsInline
+                            preload="auto"
+                            disablePictureInPicture
+                            poster="https://assets.kraken.com/illustrations/animated/posters/Beast_Web.webp"
+                        >
+                            <source
+                                type='video/mp4; codecs="hvc1"'
+                                src="https://assets.kraken.com/illustrations/animated/hevc/Beast_Web.mov"
+                            />
+
+                            <source
+                                type="video/webm"
+                                src="https://assets.kraken.com/illustrations/animated/webm/Beast_Web.webm"
+                            />
+                        </video>
                         <h2 className={'font-bold font-mono text-gray-300 text-4xl mb-4 tracking-widest'}>Авторизация</h2>
                     </div>
                     <input
@@ -40,9 +59,12 @@ export default function Room(){
                         className={`w-full
                             py-4 px-5 rounded-3xl bg-white text-black hover:bg-white/40
                             cursor-pointer font-bold font-mono capitalize tracking-wide
+                            
+                            max-[425]:absolute bottom-4 left-0 
+                            
                         `}
                     >
-                        enter
+                        enter code
                     </button>
                 </div>
             </section>
