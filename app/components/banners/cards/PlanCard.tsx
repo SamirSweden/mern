1a3c539244a6c0a0d1d7812e4d535adc84a48653
Samir Azimov <samirazimov488@gmail.com>
2026-05-26 01:52:51 +0500

    will return and create cards , now data ready

--- app/components/banners/cards/PlanCard.tsx
+++ app/components/banners/cards/PlanCard.tsx
@@ -2,7 +2,25 @@
 
 const data = [
     {
-
+        label: "Lite",
+        desc: "The indicator based on AI",
+        futures: "predicts entry point",
+        limit: "4 signals per day",
+        price: "100"
+    },
+    {
+        label: "BOSS",
+        desc: "The indicator based on AI",
+        futures: "predicts entry point , kill zones",
+        limit: "7 signals per day",
+        price: "5000"
+    },
+    {
+        label: "TURBO",
+        desc: "The indicator based on AI",
+        futures: "predicts entry point , kill zones , AMD",
+        limit: "30 signals per day",
+        price: "5000"
     },
 ];
 
