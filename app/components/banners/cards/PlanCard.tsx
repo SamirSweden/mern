bf760a14190c22de04e34a884fc68cd6188f0676
Samir Azimov <samirazimov488@gmail.com>
2026-05-26 21:32:02 +0500

    cards component is ready

--- app/components/banners/cards/PlanCard.tsx
+++ app/components/banners/cards/PlanCard.tsx
@@ -6,6 +6,7 @@ const data = [
         id: 1,
         label: "Lite",
         desc: "The indicator based on AI",
+        btnText: "purchase",
         futures: "predicts entry point",
         limit: "4 signals per day",
         price: "100",
@@ -15,6 +16,7 @@ const data = [
         id: 2,
         label: "PREMIER+",
         desc: "The indicator based on AI",
+        btnText: "purchase",
         futures: "predicts entry point , kill zones",
         limit: "7 signals per day",
         price: "2000",
@@ -24,6 +26,7 @@ const data = [
         id: 3,
         label: "TURBO",
         desc: "The indicator based on AI",
+        btnText: "purchase",
         futures: "predicts entry point , AMD",
         limit: "30 signals per day",
         price: "5000",
@@ -45,7 +48,11 @@ const PlanCard = () => {
                 `}>
 
                     <div className={`flex items-stretch gap-4 flex-col`}>
-                        <div className="relative bg-[#111] hover:bg-[#171717] w-full py-5 px-4 rounded-xl shadow-[inset_4px_4px_50px_0_hsla(0,0%,100%,.15)] ">
+                        <div className="relative w-full py-5 px-4 rounded-xl transition-all duration-300
+    bg-white/[0.03] backdrop-blur-3xl
+    border border-white/[0.08] border-t-white/[0.15] border-l-white/[0.15]
+    shadow-[inset_1px_1px_1px_0_rgba(255,255,255,0.1),_0_8px_32px_0_rgba(0,0,0,0.37)]
+    hover:bg-white/[0.06] hover:border-white/[0.2] hover:-translate-y-1 ">
                             <h2 className={'mb-3 text-white uppercase font-bold font-mono text-lg'}>{dataCard.label}</h2>
                             <div className={`flex items-center`}>
                                 <h2 className={'text-white text-3xl font-extrabold'}>{dataCard.price}</h2>
@@ -53,9 +60,20 @@ const PlanCard = () => {
                                 <span className={'text-gray-400 text-sm ml-5 absolute bottom-2 right-4'}>/{dataCard.days}</span>
                             </div>
                         </div>
-                        <h4 className={`text-gray-400 text-lg text-wrap break-words`}>{dataCard.desc}</h4>
-                        <p className={`text-white text-sm text-wrap`}>{dataCard.futures}</p>
-                        <div className={'text-gray-400 text-lg font-mono'}>
+                        <h4 className={`text-gray-400 text-sm text-wrap break-words`}>{dataCard.desc}</h4>
+
+                        <div className="flex items-center w-full ">
+                            <button className={`w-full
+                                bg-white 
+                                py-4 px-5
+                                text-center text-black text-sm font-semibold 
+                                capitalize 
+                                
+                            `}>{dataCard.btnText}</button>
+                        </div>
+
+                        <p className={`text-gray-400 font-black text-sm text-wrap`}>{dataCard.futures}</p>
+                        <div className={'text-gray-400 text-sm font-mono'}>
                             {dataCard.limit}
                         </div>
 
