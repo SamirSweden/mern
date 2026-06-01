57e0beaa555be4532ab7db0ae017a3d54204e770
Samir Azimov <samirazimov488@gmail.com>
2026-06-02 02:58:13 +0500

    added new mobile block

--- app/components/banners/banner/WeexBanner.tsx
+++ app/components/banners/banner/WeexBanner.tsx
@@ -8,13 +8,15 @@ export default function WeexBanner(){
         <>
             <section className={'relative w-full h-[90vh] overflow-hidden'}>
                 <Image src={BannerPhoto} alt={'banner'} fill priority className={'object-cover'} />
-                <div className="relative z-10 flex h-full flex-col justify-end items-start py-14 px-15">
-                   <div className={`backdrop-blur-xs bg-white/10 px-6 py-7 rounded-[60px]`}>
-                       <h1 className="text-5xl font-bold text-white">
-                           Use the best, use Kraken
+                <div className="relative z-10 flex h-full flex-col justify-end items-start p-4 sm:p-6 md:p-10">
+                   <div className={`backdrop-blur-xs bg-white/10 px-4 py-4 rounded-2xl
+                    md:rounded-[60px] sm:px-6 sm:py-5 md:px-8 md:py-7 max-w-full
+                   `}>
+                       <h1 className="text-5xl font-bold text-white max-[450px]:text-lg">
+                           Use the best &mdash; use <span className={'animate-pulse'}>Kraken</span>
                        </h1>
-                       <h2 className="text-white text-2xl font-mono font-bold">
-                           Select and win big!
+                       <h2 className="text-gray-500 text-xl mt-4 font-mono font-bold max-[450px]:text-xl">
+                           See coins and make right investment
                        </h2>
                    </div>
                 </div>
