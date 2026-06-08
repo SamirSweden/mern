10533c169ace0d55806b56910ef43888f4d2504e
Samir Azimov <samirazimov488@gmail.com>
2026-06-08 20:12:18 +0500

    added new banner img

--- app/components/UI/PreFooter.tsx
+++ app/components/UI/PreFooter.tsx
@@ -22,7 +22,7 @@ const PreFooter = () => {
                             <p className={'text-white text-lg text-wrap max-[388px]:text-center'}>{bannerContent.desc}</p>
                             <div className={'max-[420px]:w-full'}>
                                 <button
-                                    onClick={() => router.push('/start')}
+                                    onClick={() => router.push('/btc')}
                                     className={'max-[420px]:w-full bg-white text-black  py-4 px-6 hover:bg-white/70 cursor-pointer rounded-xl shadow-2xl hover:shadow-gray-400 focus:outline-none transition transform duration-300 hover:-translate-y-3 '}>
                                     {bannerContent.startBtn}
                                 </button>
--- app/components/banners/banner/WeexBanner.tsx
+++ app/components/banners/banner/WeexBanner.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
+import BtcPhoto from '@/app/assets/btc.png'
 
-
-const BannerPhoto = "https://d2tfhb4luz96yb.cloudfront.net/otc/images/mainBanner/a10ed0e89a344cc725e845e50b2fca30.webp"
 
 export default function WeexBanner(){
     return (
         <>
-            <section className={'relative w-full h-[90vh] overflow-hidden'}>
-                <Image src={BannerPhoto} alt={'banner'} fill priority className={'object-cover'} />
+            <section className={'relative w-full h-screen overflow-hidden'}>
+                <Image src={BtcPhoto} alt={'banner'} fill priority className={'object-cover object-center md:object-center'} />
+                <div className={'absolute inset-0 bg-black/20 z-10'}  />
                 <div className="relative z-10 flex h-full flex-col justify-end items-start p-4 sm:p-6 md:p-10">
                    <div className={`backdrop-blur-xs bg-white/10 px-4 py-4 rounded-2xl
                     md:rounded-[60px] sm:px-6 sm:py-5 md:px-8 md:py-7 max-w-full
                    `}>
-                       <h1 className="text-5xl font-bold text-white max-[450px]:text-lg">
+                       <h1 className="text-5xl font-bold text-gray-400 max-[450px]:text-lg">
                            Use the best &mdash; use <span className={'animate-pulse'}>Kraken</span>
                        </h1>
                        <h2 className="text-gray-500 text-xl mt-4 font-mono font-bold max-[450px]:text-xl">
